b392f322cecced500739d29efaa9a64c2f82f3e9
ozenzamy <ozenzamy@gmail.com>
2015-03-03 09:27:13 +0000

    Modifier classes

--- classes/auto_ref.class.php
+++ classes/auto_ref.class.php
@@ -8,13 +8,5 @@
 */
 class AutoRef 
 {
-   private static $duration;
-
-   public static function getDuration(){
-   	 return self::$duration;
-   } 	
-
-   public static function setDuration($value){
-   	 self::$duration = $value;
-   }
+  
 }
